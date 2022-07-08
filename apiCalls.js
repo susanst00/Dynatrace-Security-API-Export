770f9b36d33f38bf109cea5a18bdac7207d8d317
Tyler Ouellette <ouellette.tyler@hotmail.com>
2022-07-08 18:08:45 -0400

    Updated to work with new API, added CSV export for all problems

--- apiCalls.js
+++ apiCalls.js
@@ -5,7 +5,7 @@ const httpsAgent = new https.Agent({
     rejectUnauthorized: false
 });
 
-exports.makeAPICall = async (url) => {
+exports.makeAPICall = async (url, token) => {
     const response = await fetch(url, {
         method: 'GET',
         headers: {
@@ -13,25 +13,11 @@ exports.makeAPICall = async (url) => {
             'Access-Control-Allow-Origin': '*',
             'Accept-Encoding': "gzip,deflate,br",
             'Connection': 'keep-alive',
-            'Authorization': `${process.env.PROD_TOKEN}`
+            'Authorization': token
         },
         agent: httpsAgent
     }).catch(e => console.error(e));
-    return response.json();
-};
-
-exports.makeAPICallNonProd = async (url) => {
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Accept': '*/*',
-            'Access-Control-Allow-Origin': '*',
-            'Accept-Encoding': "gzip,deflate,br",
-            'Connection': 'keep-alive',
-            'Authorization': `${process.env.NONPROD_TOKEN}`
-        },
-        agent: httpsAgent
-    }).catch(e => console.error(e));
+    // return response;
     return response.json();
 };
 
